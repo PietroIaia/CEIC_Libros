5c19f36c9f0010304bd376c3b9ffb9203f2e4484
Daniela Zorrilla <38742100+DanielaZorrilla@users.noreply.github.com>
2019-10-22 18:25:19 -0400

    Add files via upload

--- Menu/Menu.py
+++ Menu/Menu.py
@@ -1,14 +1,7 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'Buttons_2.ui'
-#
-# Created by: PyQt5 UI code generator 5.13.0
-#
-# WARNING! All changes made in this file will be lost!
-
+# CEIC - Libros
+# Menu
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-
 
 class Ui_MainWindow(object):
 
