5e0b90f354b6939e9f782179ef958604c60d236e
Pietro <pietroiaia98@gmail.com>
2019-11-09 19:30:33 -0400

    Agregada ventana de prestados (No terminada)

--- prestamos.py
+++ prestamos.py
@@ -9,26 +9,38 @@ class prestamos(QWidget):
 
     def __init__(self):
 
-        #Inicialización de la ventana
+        # Inicialización de la ventana
         super().__init__()
         self.setGeometry(200, 0, 600, 600)
         self.setWindowTitle("Gestión de Estudiantes")
         self.setStyleSheet('background-color: LightSkyBlue')
 
-        #Creación de fonts para las letras
+        # Creación de fonts para las letras
         self.titleFont = QFont("Serif", 20)
         self.instFont = QFont("Serif", 12)
 
-        #Título
-        self.title = QLabel("Prestamos")
+        # Título
+        self.title = QLabel("Prestamos", self)
         self.title.setStyleSheet('background-color: DodgerBlue')
         self.title.setStyleSheet('color: white')
         self.title.setFont(self.titleFont)
+        self.title.move(30, 10)
 
-        #Instrucciones
-        self.instrucciones = QLabel("Realice o renueve prestamos y consulte prestamos activos")
-        self.instrucciones.setStyleSheet('background-color: white')
-        self.instrucciones.setFont(self.instFont)
-        self.instrucciones.setFrameShape(QFrame.StyledPanel)
-        self.instrucciones.setFrameShadow(QFrame.Plain)
-        self.instrucciones.setLineWidth(0)
+        # Frame del form
+        frame_form_prestamo = QFrame(self)
+        frame_form_prestamo.setFrameShape(QFrame.StyledPanel)
+        frame_form_prestamo.setFixedWidth(300)
+        frame_form_prestamo.setFixedHeight(370)
+        frame_form_prestamo.setStyleSheet("QFrame \n"
+        "{\n"
+        "background-color: white;\n"
+        "}")
+        frame_form_prestamo.move(30, 60)
+
+        # Carnet de estudiante #carnet del estudiante
+        self.currentStudent = "" #GUarda el valor del carnet del estudiante actualmente mostrado en pantalla
+        self.carnetLabel = QLabel("Número de carnet: ", frame_form_prestamo)
+        self.carnetLabel.move(10, 10)
+        self.carnet = QLineEdit(frame_form_prestamo)
+        self.carnet.setStyleSheet('background-color: white')
+        self.carnet.move(10, 30)
