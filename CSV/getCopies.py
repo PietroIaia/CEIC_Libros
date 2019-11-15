0e4b013b13b7b1f5572ef8c7900213f8fbba75c7
Diego <djpg98@gmail.com>
2019-11-14 22:08:00 -0400

    Hoja csv copias arreglada

--- CSV/getCopies.py
+++ CSV/getCopies.py
@@ -7,7 +7,7 @@ with open('Codes.csv', newline='\n') as csvfile:
     reader.__next__()
     for row in reader:
         for i in range(int(row[2])):
-            ejemplares.append([i, row[3]])
+            ejemplares.append([i + 1, row[3]])
 csvfile.closed
 
 with open('CopyCodes.csv', 'w', newline='\n') as csvfile:
