151f9c01c582483d49080d51f940e02c3814dc01
Daniela Zorrilla <38742100+DanielaZorrilla@users.noreply.github.com>
2019-11-28 12:17:10 -0400

    Cambios interfaz

--- gestionUsuarios.py
+++ gestionUsuarios.py
@@ -6,6 +6,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QFont, QPixmap, QColor
 from PyQt5.QtCore import pyqtSlot, Qt, QSize
 from PyQt5.QtSql import QSqlDatabase, QSqlQuery
+from PyQt5 import QtCore, QtGui, QtWidgets
 from Tables import UserTable
 from Prompt import ErrorPrompt, InfoPrompt, ConfirmPrompt
 from validationFunctions import verification_users, check_username
@@ -18,73 +19,113 @@ class gestionUsuarios(QWidget):
 
     def __init__(self, loggedUser):
 
-        #Inicialización de la ventana
+        # Inicialización de la ventana
         super().__init__()
         self.setGeometry(200, 0, 600, 600)
         self.setWindowTitle("Gestión de Usuarios")
-        self.setStyleSheet("* \n"
-        "{\n"
-        "background-color: LightSkyBlue;\n"
-        "}\n"
-        "QLineEdit\n"
-        "{\n"
-        "background-color: white;\n"
-        "}\n"
-        "QPushButton\n"
-        "{\n"
-        "background-color: PowderBlue;\n"
-        "}")
+        self.setStyleSheet('background-color: rgb(236, 240, 241)')
         self.old_perm_mask = ""
 
-        #Creación de fonts para las letras
+        # Creación de fonts para las letras
         self.titleFont = QFont("Serif", 20)
+        self.titleFont.setBold(True)
         self.instFont = QFont("Serif", 12)
+        self.buttonFont = QFont("Arial", 11)
+        self.buttonFont.setBold(True)
 
-        #Título
-        self.title = QLabel("Gestión de Usuarios")
-        self.title.setStyleSheet('color: white')
+        # Título
+        self.title = QtWidgets.QLabel(self)
+        self.title.setText("Gestión de Usuarios")
+        self.title.setStyleSheet('color: rgb(30, 39, 46)')
         self.title.setFont(self.titleFont)
+        self.title.setGeometry(10, 15, 350, 50)
 
-        #Instrucciones
-        self.instrucciones = QLabel("Ingrese el username del usuario a consultar, ingresar o modificar")
-        self.instrucciones.setStyleSheet('background-color: white')
-        self.instrucciones.setFont(self.instFont)
-        self.instrucciones.setFrameShape(QFrame.StyledPanel)
-        self.instrucciones.setFrameShadow(QFrame.Plain)
-        self.instrucciones.setLineWidth(0)
+        # Línea debajo del título
+        self.line = QtWidgets.QFrame(self)
+        self.line.setGeometry(QtCore.QRect(10, 55, 820, 16))
+        self.line.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line.setObjectName("line")
 
-        #Tabla donde aparecerán los datos
+        # Tabla donde aparecerán los datos
         self.table = UserTable() #Tablas
 
-        #Botones de agregar, modificar, cancelar y confirmar (Acciones)
-        self.modificar = QPushButton("Modificar")
-        self.guardar = QPushButton("Guardar modificación")
-        self.cancel = QPushButton("Cancelar modificación")
-        self.eliminar = QPushButton("Eliminar Usuario")
-        self.confirm = QPushButton("Confirmar eliminación")
-        self.deleteCancel = QPushButton("Cancelar eliminación")
+        # Botones de modificar, eliminar, cancelar y confirmar
+        self.modificar = QPushButton(self)
+        self.guardar = QPushButton(self)
+        self.cancel = QPushButton(self)
+        self.eliminar = QPushButton(self)
+        self.confirm = QPushButton(self)
+        self.deleteCancel = QPushButton(self)
+
+        # Se esconden los botones
         self.confirm.hide()
         self.deleteCancel.hide()
+        self.guardar.hide()
+        self.cancel.hide()
 
-        #Colores de los botones
-        self.modificar.setStyleSheet('background-color: PowderBlue')
-        self.cancel.setStyleSheet('background-color: PowderBlue')
-        self.guardar.setStyleSheet('background-color: PowderBlue')
-        self.eliminar.setStyleSheet('background-color: PowderBlue')
-        self.confirm.setStyleSheet('background-color: Red; color: white')
-        self.deleteCancel.setStyleSheet('background-color: Red; color: white')
+        # Ícono para el botón modificar
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap("static/edit-ceic-user-white.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.modificar.setIcon(icon1)
+        self.modificar.setIconSize(QtCore.QSize(40, 30))
+        self.modificar.setFlat(True)
+
+        # Ícono para el botón eliminar
+        icon2 = QtGui.QIcon()
+        icon2.addPixmap(QtGui.QPixmap("static/delete-ceic-user-white.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.eliminar.setIcon(icon2)
+        self.eliminar.setIconSize(QtCore.QSize(25, 25))
+        self.eliminar.setFlat(True)
+
+        # Texto de los botones
+        self.modificar.setText("      Modificar")
+        self.guardar.setText("Guardar")
+        self.cancel.setText("Cancelar")
+        self.eliminar.setText("         Eliminar")
+        self.confirm.setText("Confirmar")
+        self.deleteCancel.setText("Cancelar")
+
+        # CSS de los botones
+        self.set_button_style_sheet("modificar", "#2D98DA", "#238ED0", "#C8C8C8", "17px")
+        self.set_button_style_sheet("cancel", "#55c1ff", "#41acee", "#4bb6f8", "15px")
+        self.set_button_style_sheet("guardar", "#55c1ff", "#41acee", "#4bb6f8", "15px")
+        self.set_button_style_sheet("eliminar", "#C20000", "#CC0000", "#C8C8C8", "17px")
+        self.set_button_style_sheet("confirm", "#b80000", "#CC0000", "#D60000", "15px")
+        self.set_button_style_sheet("deleteCancel", "#b80000", "#CC0000", "#D60000", "15px")
+
+        # Cursor para los botones
+        self.modificar.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.cancel.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.guardar.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.eliminar.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.confirm.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.deleteCancel.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+
+        # Posición para los botones
+        self.modificar.setGeometry(657, 150, 180, 35)
+        self.guardar.setGeometry(657, 535, 180, 30)
+        self.cancel.setGeometry(657, 570, 180, 30)
+        self.eliminar.setGeometry(657, 190, 180, 35)
+        self.confirm.setGeometry(657, 535, 180, 30)
+        self.deleteCancel.setGeometry(657, 570, 180, 30)
+
+        # Font de los botones
+        self.modificar.setFont(self.buttonFont)
+        self.cancel.setFont(self.buttonFont)
+        self.guardar.setFont(self.buttonFont)
+        self.eliminar.setFont(self.buttonFont)
+        self.confirm.setFont(self.buttonFont)
+        self.deleteCancel.setFont(self.buttonFont)
         
-        #Configuración del layout
+        # Configuración del layout
         self.actionsLayout = QVBoxLayout()
-        self.actionsLayout.addWidget(self.modificar)
-        self.actionsLayout.addWidget(self.guardar)
-        self.actionsLayout.addWidget(self.cancel)
-        self.actionsLayout.addWidget(self.eliminar)
-        self.actionsLayout.addWidget(self.confirm)
-        self.actionsLayout.addWidget(self.deleteCancel)
+        self.actionsLayout.addStretch()
+        self.widget = QWidget()
+        self.actionsLayout.addWidget(self.widget)
         self.actionsLayout.addStretch()
 
-        #Desactivar botones
+        # Desactivar botones
         self.modificar.setEnabled(False)
         self.guardar.setEnabled(False)
         self.cancel.setEnabled(False)
@@ -92,41 +133,47 @@ class gestionUsuarios(QWidget):
         self.confirm.setEnabled(False)
         self.deleteCancel.setEnabled(False)
 
-        #Ponemos la tabla y los botones en un mismo LAyout
+        # Ponemos la tabla y los botones en un mismo LAyout
         self.tableLayout = QHBoxLayout()
         self.tableLayout.addWidget(self.table)
         self.tableLayout.addLayout(self.actionsLayout)
+        self.setLayout(self.tableLayout)
 
-        #username de usuario
-        self.currentUser = "" #GUarda el valor del username del usuario actualmente mostrado en pantalla
-        self.UserLabel = QLabel("Nombre de usuario: ")
+        # Username de usuario
+        self.currentUser = ""   # Guarda el valor del username del usuario actualmente mostrado en pantalla
+        
+        self.UserLabel = QtWidgets.QLabel(self)
+        self.UserLabelFont = QFont("Arial", 12)
+        self.UserLabelFont.setBold(True)
+        self.UserLabel.setFont(self.UserLabelFont)
+        self.UserLabel.setText("Usuario ")
+        self.UserLabel.setStyleSheet('color: rgb(72, 84, 96)')
+        self.UserLabel.setGeometry(10, 645, 500, 30)
+        
+        # Edit para ingresar el username del usuario
         self.User = QLineEdit(self)
-        self.infoLayout = QHBoxLayout()
-        self.infoLayout.addWidget(self.UserLabel)
-        self.infoLayout.addWidget(self.User)
+        self.User.setStyleSheet('background-color: white; \n border-radius: 10px; border: 1px solid rgb(190, 198, 206); font: 12px;')
+        self.User.setPlaceholderText(" Ingrese el username del usuario a consultar, modificar o eliminar")
+        self.User.setGeometry(85, 645, 565, 30)
 
-        #botones de consulta y agregar
-        self.search = QPushButton("Consultar")
-        self.nuevo = QPushButton("Agregar nuevo usuario")
-        self.search.setStyleSheet('background-color: PowderBlue')
-        self.nuevo.setStyleSheet('background-color: PowderBlue')
-        self.searchLayout = QVBoxLayout()
-        self.searchLayout.addLayout(self.infoLayout)
-        self.searchLayout.addWidget(self.search)
-        self.searchLayout.addWidget(self.nuevo)
+        # Botón de consulta
+        self.search = QPushButton(self)
+        self.set_button_style_sheet("search", "#37A2E4", "#238ED0", "#2D98DA", "15px")
+        self.search.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.search.setText("Consultar")
+        self.search.setGeometry(85, 680, 565, 30)
+        self.search.setFont(self.buttonFont)
         self.search.setEnabled(False)
 
-        #Layout del texto
-        self.textLayout = QVBoxLayout()
-        self.textLayout.addWidget(self.title)
-        self.textLayout.addWidget(self.instrucciones)
-        self.textLayout.addStretch()
-        self.textLayout.addLayout(self.tableLayout)
-        self.textLayout.addStretch()
-        self.textLayout.addLayout(self.searchLayout)
-    
-        self.setLayout(self.textLayout)
+        # Conexiones de los botones para cambiar colores
+        self.modificar.clicked.connect(lambda: self.set_button_style_sheet("modificar", "#2D98DA", "#238ED0", "#2D98DA", "17px"))
+        self.guardar.clicked.connect(lambda: self.set_button_style_sheet("modificar", "#2D98DA", "#238ED0", "#C8C8C8", "17px"))
+        self.cancel.clicked.connect(lambda: self.set_button_style_sheet("modificar", "#2D98DA", "#238ED0", "#C8C8C8", "17px"))
+        self.eliminar.clicked.connect(lambda: self.set_button_style_sheet("eliminar", "#C20000", "#CC0000", "#C20000", "17px"))
+        self.confirm.clicked.connect(lambda: self.set_button_style_sheet("eliminar", "#C20000", "#CC0000", "#C8C8C8", "17px"))
+        self.deleteCancel.clicked.connect(lambda: self.set_button_style_sheet("eliminar", "#C20000", "#CC0000", "#C8C8C8", "17px"))
 
+        # Conexiones de los botones
         self.search.clicked.connect(self.consulta)
         self.modificar.clicked.connect(self.update)
         self.cancel.clicked.connect(self.cancelUpdate)
@@ -134,14 +181,31 @@ class gestionUsuarios(QWidget):
         self.eliminar.clicked.connect(self.deleteRequest)
         self.confirm.clicked.connect(self.deleteConfirm)
         self.deleteCancel.clicked.connect(self.cancelDelete)
-        self.nuevo.clicked.connect(self.addUser)
         self.User.textChanged[str].connect(self.check_disable)
 
+    # Función para cambiar el stylesheet de los botones
+    def set_button_style_sheet(self, button_id, hover_color, pressed_color, bg_color, radius):
+        button = getattr(self, '%s' %button_id)
+        button.setStyleSheet("QPushButton:hover\n"
+                "{\n"
+                "    background-color: " + hover_color + ";\n"
+                "}\n"
+                "\n"
+                "QPushButton:pressed\n"
+                "{\n"
+                "    background-color: " + pressed_color + ";\n"
+                "}\n"
+                "QPushButton\n"
+                "{\n"
+                "    background-color: " + bg_color + ";\n"
+                "    border-radius: " + radius + ";\n"
+                "    color: white;\n"
+                "}")
 
     def consulta(self):
         inputUser = self.User.text()
 
-        if(checkUsername(inputUser)):
+        if(check_username(inputUser)):
             self.consultaAux(inputUser)
         else:
             ErrorPrompt("Error de usuario", "El username no sigue el patrón correspondiente")
@@ -183,11 +247,12 @@ class gestionUsuarios(QWidget):
         self.table.item(5, 0).setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)                         # No deja modificar la fila "Ultima conexion"
         self.table.item(6, 0).setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)                         # No deja modificar la fila "Fecha de creacion"
         self.search.setEnabled(False)
-        self.nuevo.setEnabled(False)
         self.eliminar.setEnabled(False)
         self.modificar.setEnabled(False)
         self.guardar.setEnabled(True)
         self.cancel.setEnabled(True)
+        self.guardar.show()
+        self.cancel.show()
         InfoPrompt("Modificación activada", "Se ha activado el modo modificación")
 
     @pyqtSlot()
@@ -215,11 +280,12 @@ class gestionUsuarios(QWidget):
         if self.query.first():
             InfoPrompt("Actualización completada", "La información del usuario ha sido actualizada exitosamente")
             self.search.setEnabled(True)
-            self.nuevo.setEnabled(True)
             self.eliminar.setEnabled(True)
             self.modificar.setEnabled(True)
             self.guardar.setEnabled(False)
             self.cancel.setEnabled(False)
+            self.guardar.hide()
+            self.cancel.hide()
             self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
             self.table.cellWidget(4, 0).setEnabled(False)
         else:
@@ -230,11 +296,12 @@ class gestionUsuarios(QWidget):
         InfoPrompt("Modificación cancelada", "Los cambios hechos no fueron guardados")
         self.consultaAux(self.currentUser)
         self.search.setEnabled(True)
-        self.nuevo.setEnabled(True)
         self.eliminar.setEnabled(True)
         self.modificar.setEnabled(True)
         self.guardar.setEnabled(False)
         self.cancel.setEnabled(False)
+        self.guardar.hide()
+        self.cancel.hide()
         self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
         self.table.cellWidget(4, 0).setEnabled(False)
 
@@ -242,8 +309,8 @@ class gestionUsuarios(QWidget):
     def deleteRequest(self):
         ConfirmPrompt("Eliminación de usuario", "Se ha solicitado eliminar usuario. Marque el botón de confirmar eliminación para eliminar el usuario o el botón de cancelar eliminación para regresar")
         self.search.setEnabled(False)
-        self.nuevo.setEnabled(False)
         self.modificar.setEnabled(False)
+        self.eliminar.setEnabled(False)
         self.confirm.setEnabled(True)
         self.deleteCancel.setEnabled(True)
         self.confirm.show()
@@ -256,7 +323,6 @@ class gestionUsuarios(QWidget):
         self.query = QSqlQuery()
         self.query.exec_(queryText)
         self.search.setEnabled(True)
-        self.nuevo.setEnabled(True)
         self.modificar.setEnabled(False)
         self.eliminar.setEnabled(False)
         self.confirm.hide()
@@ -269,18 +335,12 @@ class gestionUsuarios(QWidget):
     @pyqtSlot()
     def cancelDelete(self):
         self.search.setEnabled(True)
-        self.nuevo.setEnabled(True)
         self.modificar.setEnabled(True)
+        self.eliminar.setEnabled(True)
         self.confirm.setEnabled(False)
         self.deleteCancel.setEnabled(False)
         self.confirm.hide()
         self.deleteCancel.hide()
-
-    @pyqtSlot()
-    def addUser(self):
-        self.form = AgregarUsuario()
-        self.form.show()
-        
 
     @pyqtSlot()
     def check_disable(self):
@@ -289,10 +349,9 @@ class gestionUsuarios(QWidget):
         else:
             self.search.setEnabled(True)
                 
+#if __name__ == '__main__':
+#    app = QApplication()
 
-if __name__ == '__main__':
-    app = QApplication()
-
-    estudiante = gestionUsuarios()
-    estudiante.show()
-    sys.exit(app.exec_())
+#    estudiante = gestionUsuarios()
+#    estudiante.show()
+#    sys.exit(app.exec_())
